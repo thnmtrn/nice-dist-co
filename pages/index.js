b5d9c833754d3d926386d2fc0462a7df532888bc
thnmtrn <thien.tran6@gmail.com>
2021-03-11 00:27:50 -0500

    inventory change

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ const Home = ({ inventoryData = [], categories: categoryData = [] }) => {
           <div className="pt-4 pl-2 sm:pt-12 sm:pl-12 flex flex-col">
             <Tag
               year="2021"
-              category="SOFAS"
+              category="Gel"
             />
             <Center
               price="200"
@@ -31,7 +31,7 @@ const Home = ({ inventoryData = [], categories: categoryData = [] }) => {
               link={`/product/${slugify(inventory[2].name)}`}
             />
             <Footer
-              designer="Jason Bourne"
+              designer="Thien T."
             />
           </div>
           <div className="flex flex-1 justify-center items-center relative">
@@ -64,7 +64,7 @@ const Home = ({ inventoryData = [], categories: categoryData = [] }) => {
       </div>
       <div className="pt-10 pb-6 flex flex-col items-center">
         <h2 className="text-4xl mb-3">Trending Now</h2>
-        <p className="text-gray-600 text-sm">Find the perfect piece or accessory to finish off your favorite room in the house.</p>
+        <p className="text-gray-600 text-sm">Best premium beauty products</p>
       </div>
       <div className="my-8 flex flex-col lg:flex-row justify-between">
         <DisplaySmall
